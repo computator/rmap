351493e65edb722517a036144eaa2cedd20f3abe
Reuben Lifshay <rlifshay@gmail.com>
2017-11-10 04:50:31 -0800

    Add usage and a quiet option.

--- rmap.sh
+++ rmap.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
 HG=${HG:-hg}
 MAPNAME=${MAPNAME:-.repomap}
-command=${1:?Command is required}
+
+usage () {
+	self=$(basename "$0")
+	cat <<-HELP
+		Usage: $self [-q] [options] [--] clone <prefix> [arg] ...
+		       $self [-q] [options] [--] <command> [arg] ...
+
+		Options:
+		  -q,  --quiet		Don't show the current repo header before running commands
+
+	HELP
+	exit 2
+}
+
+opts=$(getopt -n $(basename "$0") -s sh -o +rqh -l recursive,quiet,help -- "$@")
+[ $? -eq 0 ] || usage
+eval set -- "$opts"
+for opt; do [ "$opt" = "-h" -o "$opt" = "--help" ] && usage; [ "$opt" = "--" ] && break; done
+unset quiet
+while [ "$1" != "--" ]; do
+	case "$1" in
+		-q|--quiet)
+			quiet=1 ;;
+	esac
+	shift
+done
+shift
+
+[ -n "$1" ] || usage
+command="$1"
 shift
 if [ "$command" = "clone" ]; then
 	prefix=${1:?Prefix is required}
@@ -22,7 +51,7 @@ exec 3< "$ROOT/$MAPNAME" # assign the map file to fd 3
 while read target src repo <&3 || [ -n "$target" ]; do
 	[ -n "$target" ] || [ "${target}" = "#${target#?}" ] || continue
 
-	echo "--- $target ---"
+	[ -z $quiet ] && echo "--- $target ---"
 	if [ "$command" = "clone" ]; then
 		[ -n "$src" ] || src="$target"
 		[ -n "$repo" ] || repo="$prefix"
