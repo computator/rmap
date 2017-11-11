5a1bf992d9f95335def92efd545ee874fb12987d
Reuben Lifshay <rlifshay@gmail.com>
2017-11-11 04:00:15 -0800

    Recursion starts from the root.

--- rmap.sh
+++ rmap.sh
@@ -99,7 +99,7 @@ if [ ! $subrecurse ] && [ $recurse ]; then
 	trap "rm -f '$pfile'" EXIT
 	while true; do
 		nlines=$(wc -l "$pfile" | cut -f 1 -d " ")
-		for path in $(find -depth -mindepth 2 -type f -name "$MAPNAME" | tac); do
+		for path in $(find $ROOT -depth -mindepth 2 -type f -name "$MAPNAME" | tac); do
 			path=$(dirname "$path")
 			grep -qFxe "$path" "$pfile" && continue
 			ROOT="$path" "$0" --recurse-internal
